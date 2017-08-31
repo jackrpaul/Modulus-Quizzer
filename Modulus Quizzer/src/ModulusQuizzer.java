c37c41eb38575f015f4221464fea92a2ea7d29c1
Jackson.Paul <Jackson.Paul@MU-C1-12.mullen.pvt.k12.co.us>
2017-08-31 14:02:56 -0600

    initial commit

---
+++ Modulus Quizzer/src/ModulusQuizzer.java
@@ -0,0 +1,30 @@
+
+public class ModulusQuizzer
+	{
+
+		public static void main(String[] args)
+			{
+			greetUser();
+			generateQuestions();
+			quizUser();
+			}
+
+		private static void greetUser()
+			{
+			
+				
+			}
+
+		private static void generateQuestions()
+			{
+				
+				
+			}
+
+		private static void quizUser()
+			{
+				
+				
+			}
+
+	}
